31184f6e7519e4dc754bbdceeee3be37c489de50
kkkovalov <vlad_kovalov@icloud.com>
2023-10-05 17:53:42 -0700

    Enumarated structure with class identifier

---
+++ enums/index.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+enum class Animal
+{
+    pig,
+    chicken,
+    goat,
+    cat,
+    dog,
+    duck
+};
+
+constexpr std::string_view getAnimalName(Animal animal)
+{
+    switch (animal)
+    {
+    case Animal::pig:
+        return "pig";
+    case Animal::chicken:
+        return "chicken";
+    case Animal::goat:
+        return "goat";
+    case Animal::cat:
+        return "cat";
+    case Animal::dog:
+        return "dog";
+    case Animal::duck:
+        return "duck";
+
+    default:
+        return "???";
+    };
+}
+
+void printNumberOfLegs(Animal animal)
+{
+    std::cout << "A " << getAnimalName(animal) << " has ";
+    switch (animal)
+    {
+
+    case Animal::chicken:
+    case Animal::duck:
+        std::cout << 2;
+        break;
+
+    case Animal::goat:
+    case Animal::pig:
+    case Animal::cat:
+    case Animal::dog:
+        std::cout << 4;
+        break;
+
+    default:
+        std::cout << "???";
+        break;
+    }
+    std::cout << " legs.\n";
+}
+
+int main()
+{
+    printNumberOfLegs(Animal::cat);
+    printNumberOfLegs(Animal::goat);
+
+    return 0;
+}
