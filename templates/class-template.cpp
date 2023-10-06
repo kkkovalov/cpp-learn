824dc8cfae429ae39f77c093dc1434a922b93d3f
kkkovalov <vlad_kovalov@icloud.com>
2023-10-05 19:11:29 -0700

    Add std::pair example and function template

--- templates/class-template.cpp
+++ templates/class-template.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include <utility>
 
-template <typename T>
+template <typename T, typename U>
 struct Pair
 {
     T first{};
-    T second{};
+    U second{};
 };
+
+template <typename T, typename U>
+void print(std::pair<T, U> p)
+{
+    std::cout << '[' << p.first << ", " << p.second << "]\n";
+}
 
 int main()
 {
-    Pair<int> p1{5, 6};
+    Pair<int, int> p1{5, 6};
     std::cout << p1.first << ' ' << p1.second << '\n';
-    Pair<double> p2{5.2, 6.1};
+    Pair<double, double> p2{5.2, 6.1};
     std::cout << p2.first << ' ' << p2.second << '\n';
+    std::pair<int, int> p3{1, 2};
+    print(p3);
+    std::pair<double, double> p4{3.123, 8.12};
+    print(p4);
 }
