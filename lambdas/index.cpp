5cf99560a60aee779bf0b08810fe35e91eab27e4
kkkovalov <vlad_kovalov@icloud.com>
2023-11-03 18:43:03 -0700

    Ascending temperature using lambda

--- lambdas/index.cpp
+++ lambdas/index.cpp
@@ -1,28 +1,29 @@
-#include <iostream>
+#include <algorithm>
 #include <array>
-#include <string>
+#include <iostream>
+#include <string_view>
 
-struct Student {
-    std::string name;
-    int points;
+struct Season
+{
+  std::string_view name{};
+  double averageTemperature{};
 };
 
-bool max_elem (int a, int b) {
-    return a < b;
+int main()
+{
+  std::array<Season, 4> seasons{
+    { { "Spring", 285.0 },
+      { "Summer", 296.0 },
+      { "Fall", 288.0 },
+      { "Winter", 263.0 } }
+  };
+
+    std::sort(seasons.begin(), seasons.end(), [](Season a, Season b){ return a.averageTemperature < b.averageTemperature; });
+
+  for (const auto& season : seasons)
+  {
+    std::cout << season.name << '\n';
+  }
+
+  return 0;
 }
-
-int main () {
-    std::array<Student, 8> arr = { {
-    { "Albert", 3 },
-    { "Ben", 5},
-    { "Christine", 2 },
-    { "Dan", 8 }, // Dan has the most points (8).
-    { "Enchilada", 4 },
-    { "Francis", 1 },
-    { "Greg", 3 },
-    { "Hagrid", 5 }
-    } };
-
-    std::cout << std::max_element (arr.begin (), arr.end (), [](Student a, Student b) {return a.points < b.points;})->name << " is the best student.\n";
-
-};
